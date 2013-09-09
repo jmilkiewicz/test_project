5daa9f660491044a185c1a07ed6ce91c3ff3fa81
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2013-09-09 19:43:41 +0200

    adding Spring

--- src/main/java/MyBean.java
+++ src/main/java/MyBean.java
@@ -1,4 +1,19 @@
 
 public class MyBean {
+    private String home;
 
+    public String getHome() {
+        return home;
+    }
+
+    public void setHome(String home) {
+        this.home = home;
+    }
+
+    @Override
+    public String toString() {
+        return "MyBean{" +
+                "home='" + home + '\'' +
+                '}';
+    }
 }
