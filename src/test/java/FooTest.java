53e202257746b577b2ab7e222e9ff66bc9eb07e4
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2013-09-09 18:28:09 +0200

    sdasda

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -38,8 +38,6 @@ public class FooTest {
             System.out.println(objectObjectEntry.getKey() + "=" + objectObjectEntry.getValue());
         }
 
-
-
         System.out.println("++++ ENV Variables ++++++");
         Map<String, String> getenv = System.getenv();
         for (Map.Entry<String, String> stringStringEntry : getenv.entrySet()) {
