4f5cc0d33a63986328edabf302af2e955ee71995
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2013-09-25 22:29:58 +0200

    sda

--- src/test/java/EnvPropTest.java
+++ src/test/java/EnvPropTest.java
@@ -1,5 +1,9 @@
 import org.junit.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Properties;
 
@@ -7,7 +11,7 @@ public class EnvPropTest {
 
 
     @Test
-    public void foo(){
+    public void foo() throws IOException {
         Properties properties = System.getProperties();
         System.out.println("++++ SYSTEM PROPERTIES ++++++");
         for (Map.Entry<Object, Object> objectObjectEntry : properties.entrySet()) {
@@ -19,5 +23,12 @@ public class EnvPropTest {
         for (Map.Entry<String, String> stringStringEntry : getenv.entrySet()) {
             System.out.println(stringStringEntry.getKey() + "=" + stringStringEntry.getValue());
         }
+
+
+        String kuba = System.getenv("KUBA");
+        FileWriter fileOutputStream = new FileWriter("aaa.txt");
+        fileOutputStream.write(kuba);
+        fileOutputStream.flush();
+        fileOutputStream.close();
     }
 }
