940d87ca7fc9c4df638f6cc2689981bb04fcfc66
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2014-03-11 21:40:16 +0100

    asdasda

--- src/main/java/MyBean.java
+++ src/main/java/MyBean.java
@@ -3,6 +3,7 @@ public class MyBean {
     private String home;
     private String home2;
     private String home3;
+    private String home4;
 
     public String getHome() {
         return home;
