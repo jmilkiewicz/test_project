0cb1508d79d0a2f8fcc6934a9809dfeed8795018
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2014-03-12 08:31:03 +0100

    212

--- src/main/java/MyBean.java
+++ src/main/java/MyBean.java
@@ -1,29 +1,6 @@
 
 public class MyBean {
     private String home;
-    private String home2;
-    private String home4;
-    private String home5;
-    private String home6;
-    private String home7;
-    private String home8;
-    private String home9;
-    private String home10;
-    private String home11;
-    private String home12;
-    private String home13;
-    private String home14;
-    private String home15;
-    private String home16;
-    private String home17;
-    private String home18;
-    private String home19;
-    private String home20;
-    private String home21;
-    private String home22;
-    private String home23;
-    private String home24;
-    private String home25;
     private String home26;
     private String home27;
     private String home87;
